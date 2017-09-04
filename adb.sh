56d74ab4fa96211e6a8a4ad8d9d140b19f952704
nosaka <nosaka.shin@gmail.com>
2017-09-04 12:00:28 +0900

    modify script.

--- adb.sh
+++ adb.sh
@@ -2,9 +2,11 @@
 
 # constant
 DIR_TOOL="./tools/"
+DIR_TEMPLATE="./template/"
 DIR_WORK="./work/"
 DIR_DECOMP="./decomp/"
-DIR_DECOMP_JAVA=$DIR_DECOMP"main/java/"
+DIR_DECOMP_SOURCE=$DIR_DECOMP"app/src/main/"
+DIR_DECOMP_SOURCE_JAVA=$DIR_DECOMP_SOURCE"java/"
 
 APK_FILE=$DIR_WORK"target.apk"
 APK_JAR=$DIR_WORK"classes-dex2jar.jar"
@@ -25,17 +27,18 @@ echo -n "Please input package name."
 read in_package
 
 # 端末内パッケージの検索
-echo "Search [$in_package]"
+echo "Search [$in_package] from the device."
 pm_result=`adb shell pm list packages -f | grep $in_package | head -1`
+
+if [ ! -n "$pm_result" ]; then
+	echo "[$in_package] was not found." >&2
+	exit
+fi
 
 echo "================"
 echo "$pm_result"
 echo "================"
 
-if [ ! -n "$pm_result" ]; then
-	echo "[$in_package] was not found."
-	exit
-fi
 
 # 対象パッケージの確認
 target_pkg=`echo $pm_result | sed -e "s/^.*=\(.*\).*$/\1/"`
@@ -46,8 +49,7 @@ echo -n "Analyze [$target_pkg]? y/n"
 read is_analyze
 
 case $is_analyze in 
-	y)
-		echo 'continue...'
+	y*)
 		;;
 	*)
 		echo 'Exit.'
@@ -56,20 +58,30 @@ case $is_analyze in
 	esac
 
 # 端末からapkの抜き出し
-adb pull $target_apk $APK_FILE
+echo 'Pull out apk from device.'
+adb pull $target_apk $APK_FILE >/dev/null
+
+echo 'Please wait a moment...'
 
 # apktoolの実行
-$CMD_APKTOOL decode --no-src $APK_FILE -o  $DIR_DECOMP -f 
+$CMD_APKTOOL decode --no-src $APK_FILE -o $DIR_DECOMP_SOURCE -f >/dev/null 2>&1
 
 # dex2jarの実行
-$CMD_D2J $DIR_DECOMP"classes.dex" -o $APK_JAR --force  > /dev/null
+$CMD_D2J $DIR_DECOMP_SOURCE"classes.dex" -o $APK_JAR --force >/dev/null 2>&1
 
 # jarの解凍
 unzip $APK_JAR  -d $DIR_WORK > /dev/null
 
 # jadの実行
-mkdir -p $DIR_DECOMP_JAVA
-jad -o -r -sjava -d$DIR_DECOMP_JAVA $DIR_WORK"**/*.class" > /dev/null
+mkdir -p $DIR_DECOMP_SOURCE
+jad -o -r -sjava -d$DIR_DECOMP_SOURCE_JAVA $DIR_WORK"**/*.class" >/dev/null 2>&1
+
+# gradleファイルのコピー
+cp $DIR_TEMPLATE"settings.gradle" $DIR_DECOMP"settings.gradle"
+cp $DIR_TEMPLATE"pj_build.gradle" $DIR_DECOMP"build.gradle"
+cp $DIR_TEMPLATE"app_build.gradle" $DIR_DECOMP"app/build.gradle"
+
+echo '★★★★ Success ★★★★'
 
 # 後処理
 rm -rf $DIR_WORK
